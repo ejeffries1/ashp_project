6f9a69714aa2adf90f5967adf88ad78ecc4697f0
Evan Jeffries <ejeffries14@gmail.com>
2019-10-28 21:19:28 +0000

    environment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,10 @@
 class Scraper
   
   def current_drugs
-    N
+    @doc = NOKOGIRI::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages"))
+    @doc.css(".main main-container").each do |drugs|
+      
+    end
+    binding.pry
   end
 end
