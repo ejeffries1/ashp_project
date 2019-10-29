e3377dae8f75d856c9c98bfb4d68163025ffc642
Evan Jeffries <ejeffries14@gmail.com>
2019-10-29 14:38:35 +0000

    modified scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ class Scraper
   def current_drugs
     @doc = NOKOGIRI::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages"))
     @doc.css(".main main-container").each do |drugs|
-      
+      drugs.css(".drug-shortage-container")
     end
     binding.pry
   end
